e630fb6947f00c3c81e653365b92c3d28e27e384
Zericy <58168039+Zericy@users.noreply.github.com>
2020-05-08 14:56:48 +0200

    Added StartScreen

---
+++ Assets/Scripts/Buttons/ButtonsBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonsBehaviour : MonoBehaviour
+{
+    public string StartingPanel;
+    public GameObject[] Panels;
+    private void Start()
+    {
+        ChangePanel(StartingPanel);
+    }
+    public void LoadScene(string SceneToLoad)
+    {
+        SceneManager.LoadScene(SceneToLoad);
+    }
+
+    public void ChangePanel(string name)
+    {
+        foreach (var item in Panels)
+        {
+            if(item.name==name)
+            {
+                item.SetActive(true);
+            }
+            else
+            {
+                item.SetActive(false);
+            }
+        }
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
