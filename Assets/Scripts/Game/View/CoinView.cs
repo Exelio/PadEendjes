e4689108cb00494c212e003775cb0b1f746900cf
Rakk19 <jensdw19@gmail.com>
2020-05-30 20:43:00 +0200

    coin script

---
+++ Assets/Scripts/Game/View/CoinView.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinView : MonoBehaviour
+{
+    private int _coinValue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _coinValue = 1;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+
+        }
+    }
+}
